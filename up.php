20d3de44a08b413c9d11254e47c6c6f960a05594
FerretSocks <102883861+FerretSocks@users.noreply.github.com>
2022-04-03 10:18:12 +1000

    Update up.php

--- up.php
+++ up.php
@@ -1,7 +1,7 @@
 <?php
-$secret_key = "KEYGOESHERE"; //Set this as your secret key, to prevent others uploading to your server.
+$secret_key = "Fb"8qrd3;=9)f6?A"; //Set this as your secret key, to prevent others uploading to your server.
 $sharexdir = "images/"; //This is your file dir, also the link..
-$domain_url = 'http://ss.yourdomain.com/'; //Add an S at the end of HTTP if you have a SSL certificate.
+$domain_url = 'http://lukederoos.com/'; //Add an S at the end of HTTP if you have a SSL certificate.
 $lengthofstring = 5; //Length of the file name
 
 function RandomString($length) {
