9356a82289c629329fba2c721f9370ecf7f88b25
FerretSocks <102883861+FerretSocks@users.noreply.github.com>
2022-04-03 10:36:54 +1000

    Update up.php

--- up.php
+++ up.php
@@ -1,7 +1,7 @@
 <?php
 $secret_key = "CobraCum"; //Set this as your secret key, to prevent others uploading to your server.
 $sharexdir = ""; //This is your file dir, also the link..
-$domain_url = 'https://lukederoos.com/'; //Add an S at the end of HTTP if you have a SSL certificate.
+$domain_url = 'https://lukederoos.com'; //Add an S at the end of HTTP if you have a SSL certificate.
 $lengthofstring = 5; //Length of the file name
 
 function RandomString($length) {
