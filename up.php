6b70461fd46730ab745ed7aa208f0119493e7d7b
FerretSocks <102883861+FerretSocks@users.noreply.github.com>
2022-04-03 10:33:52 +1000

    Update up.php

--- up.php
+++ up.php
@@ -1,6 +1,6 @@
 <?php
 $secret_key = "Fb"8qrd3;=9)f6?A"; //Set this as your secret key, to prevent others uploading to your server.
-$sharexdir = "images/"; //This is your file dir, also the link..
+$sharexdir = ""; //This is your file dir, also the link..
 $domain_url = 'https://lukederoos.com/'; //Add an S at the end of HTTP if you have a SSL certificate.
 $lengthofstring = 5; //Length of the file name
 
